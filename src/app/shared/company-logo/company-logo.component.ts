c4a2c9032f971a7bc40b4883656fa6265014d2f4
padoleshital <shitalpadole7@gmail.com>
2023-08-08 15:15:24 +0530

    added logo inl oop
    Signed-off-by: padoleshital <shitalpadole7@gmail.com>

--- src/app/shared/company-logo/company-logo.component.ts
+++ src/app/shared/company-logo/company-logo.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-company-logo',
   templateUrl: './company-logo.component.html',
   styleUrls: ['./company-logo.component.scss']
 })
-export class CompanyLogoComponent {
+export class CompanyLogoComponent implements OnInit  {
+  clientLogos:any;
+  ngOnInit(): void {
+    this.clientLogos = [
+      'amazon.svg',
+      'google.svg',
+      'lenovo.svg',
+      'paypal.svg',
+      'shopify.svg',
+      'spotify.svg'
+    ];
+  }
 
+ 
 }
